7e15678332a245dcfc48d48bb63917289ddab5a2
Jack <jackmidoglover@gmail.com>
2018-05-13 19:29:54 -0600

    troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-
 connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
